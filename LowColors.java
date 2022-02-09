1fddab2441cc14197dbca4f18f0e65bc8b313bc0
Tyranzx <70720366+Lewysan@users.noreply.github.com>
2022-02-09 17:08:35 +0000

    Add files via upload

---
+++ LowColors.java
@@ -0,0 +1,11 @@
+package yourPackage.path;
+
+import org.bukkit.*;
+
+public class LowColors(){
+
+    public static String(String text){
+        return ChatColor.translateAlternateColorCodes('&', text);
+	}
+}
+// Use these methods without constructor (static)
